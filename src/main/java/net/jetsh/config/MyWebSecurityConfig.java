2e18c453c08bf3329b63378bdda96161bbda4141
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-01-31 18:37:28 +0500

    Login page add

--- src/main/java/net/jetsh/config/MyWebSecurityConfig.java
+++ src/main/java/net/jetsh/config/MyWebSecurityConfig.java
@@ -9,7 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/mypage");
     }
 
     @Override
